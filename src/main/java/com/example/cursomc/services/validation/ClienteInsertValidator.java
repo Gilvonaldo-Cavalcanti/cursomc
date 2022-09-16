98a3a8fdc4ca13ca48306d87a88c1bb832d61d56
Gilvonaldo Cavalcanti <gilvonaldocavalcanti@gmail.com>
2022-09-16 11:40:58 -0300

    Validação de email existente

--- src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.cursomc.domain.Cliente;
 import com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.dto.ClienteNewDTO;
+import com.example.cursomc.repositories.ClienteRepository;
 import com.example.cursomc.resources.exception.FieldMessage;
 import com.example.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod())
 				&& !BR.isValidTfn(objDto.getCpfOutCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido."));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux!=null) {
+			list.add(new FieldMessage("email","Este email já está cadastrado"));
 		}
  
 		for (FieldMessage e : list) {
